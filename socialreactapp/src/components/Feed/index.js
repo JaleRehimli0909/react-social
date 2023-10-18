90f71d6644b35527ae5380d028038b9ddde419f6
JaleRehimli0909 <rehimlijale0909@gmail.com>
2023-10-18 17:51:14 +0400

    Body Component

---
+++ socialreactapp/src/components/Feed/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+const Feed = () => {
+  return (
+    <div>Feed</div>
+  )
+}
+
+export default Feed;
--- socialreactapp/src/components/Topbar/index.js
+++ socialreactapp/src/components/Topbar/index.js
@@ -1,10 +1,45 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
+import { FaSistrix, FaUserAlt, FaBell, FaStackExchange } from "react-icons/fa";
 
 const Topbar = () => {
   return (
-    <div>Topbar</div>
-  )
-}
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">RehimliSocial</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchBar">
+          <FaSistrix className="searchIcon" />
+          <input
+            placeholder="Search for friend, post or video"
+            className="serachInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">HomePage</span>
+          <span className="topbarLink">TimeLine</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <FaUserAlt />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <FaStackExchange />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <FaBell />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <img src="./assets/person/image.jpg" alt="" className="topbarImg" />
+      </div>
+    </div>
+  );
+};
 
 export default Topbar;
