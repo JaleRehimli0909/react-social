efb3516b05ce94a3d345987bdaacfa6f7d9f3a33
JaleRehimli0909 <rehimlijale0909@gmail.com>
2023-10-18 16:57:53 +0400

    Topbar Component

---
+++ socialreactapp/src/Pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+import Topbar from '../../components/Topbar';
+
+const Home = () => {
+  return (
+    <>
+    <Topbar/>
+    </>
+  )
+}
+
+export default Home;
